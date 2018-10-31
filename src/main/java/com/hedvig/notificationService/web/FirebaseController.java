38815fc5b09e863605abd4aa138391312c2f9539
meletios <meletios@hedvig.com>
2018-10-31 11:33:59 +0100

    Split controller

---
+++ src/main/java/com/hedvig/notificationService/web/FirebaseController.java
@@ -0,0 +1,66 @@
+package com.hedvig.notificationService.web;
+
+import com.hedvig.notificationService.entities.FirebaseToken;
+import com.hedvig.notificationService.service.FirebaseNotificationService;
+import java.util.Optional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/_/notifications")
+public class FirebaseController {
+
+  private final Logger log = LoggerFactory.getLogger(FirebaseController.class);
+  private final FirebaseNotificationService firebaseNotificationService;
+
+  public FirebaseController(FirebaseNotificationService firebaseNotificationService) {
+    this.firebaseNotificationService = firebaseNotificationService;
+  }
+
+  @PostMapping("/{memberId}/token")
+  public ResponseEntity<?> saveFirebaseToken(
+      @PathVariable(name = "memberId") String memberId, @RequestBody String token) {
+    try {
+      firebaseNotificationService.setFirebaseToken(memberId, token);
+    } catch (Exception e) {
+      log.error(
+          "Something went wrong while the Token {} for member {} was about to be stored in the database with error {}",
+          token,
+          memberId,
+          e);
+      return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+    }
+    return ResponseEntity.noContent().build();
+  }
+
+  @GetMapping("/{memberId}/token")
+  public ResponseEntity<?> getFirebaseToken(@PathVariable(name = "memberId") String memberId) {
+    try {
+      Optional<FirebaseToken> firebaseTokenOptional =
+          firebaseNotificationService.getFirebaseToken(memberId);
+      return firebaseTokenOptional
+          .map(firebaseToken -> ResponseEntity.ok(firebaseToken.token))
+          .orElseGet(() -> ResponseEntity.notFound().build());
+    } catch (Exception e) {
+      log.error(
+          "Something went wrong while trying to fetch the firebase token for member {} with error {}",
+          memberId,
+          e);
+      return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+    }
+  }
+
+  @PostMapping("/{memberId}/push/send")
+  public ResponseEntity<?> sendPushNotification(@PathVariable(name = "memberId") String memberId) {
+    firebaseNotificationService.sendNewMessageNotification(memberId);
+    return ResponseEntity.noContent().build();
+  }
+}
--- src/main/java/com/hedvig/notificationService/web/NotificationsController.java
+++ src/main/java/com/hedvig/notificationService/web/NotificationsController.java
@@ -2,13 +2,10 @@ package com.hedvig.notificationService.web;
 
 import com.hedvig.notificationService.dto.CancellationEmailSentToInsurerRequest;
 import com.hedvig.notificationService.dto.InsuranceActivationDateUpdatedRequest;
-import com.hedvig.notificationService.entities.FirebaseToken;
 import com.hedvig.notificationService.entities.MailConfirmation;
 import com.hedvig.notificationService.entities.MailRepository;
-import com.hedvig.notificationService.service.FirebaseNotificationService;
 import com.hedvig.notificationService.service.NotificationService;
 import java.util.Objects;
-import java.util.Optional;
 import java.util.UUID;
 import javax.validation.Valid;
 import lombok.val;
@@ -18,7 +15,6 @@ import org.slf4j.MDC;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.mail.MailException;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -32,15 +28,10 @@ public class NotificationsController {
   private final Logger log = LoggerFactory.getLogger(NotificationsController.class);
   private final NotificationService notificationService;
   private final MailRepository mailRepository;
-  private final FirebaseNotificationService firebaseNotificationService;
 
-  public NotificationsController(
-      NotificationService notificationService,
-      MailRepository mailRepository,
-      FirebaseNotificationService firebaseNotificationService) {
+  public NotificationsController(NotificationService notificationService,MailRepository mailRepository) {
     this.notificationService = notificationService;
     this.mailRepository = mailRepository;
-    this.firebaseNotificationService = firebaseNotificationService;
   }
 
   @PostMapping("/{memberId}/cancellationEmailSentToInsurer")
@@ -89,8 +80,6 @@ public class NotificationsController {
    * insurance's activation. @RequestBody NumberOfDaysFromToday the numbers from today that the
    * insurance will be activated
    *
-   * @return 204 on success
-   *     <p>or
    * @return 404 not found if there is no isnurance that will be activated on that date
    */
   @PostMapping("/insuranceWillBeActivatedAt")
@@ -109,45 +98,6 @@ public class NotificationsController {
     conf.setMemberId(memberId);
     conf.setConfirmationId(UUID.randomUUID().toString());
     mailRepository.save(conf);
-    return ResponseEntity.noContent().build();
-  }
-
-  @PostMapping("/{memberId}/token")
-  public ResponseEntity<?> saveFirebaseToken(
-      @PathVariable(name = "memberId") String memberId, @RequestBody String token) {
-    try {
-      firebaseNotificationService.setFirebaseToken(memberId, token);
-    } catch (Exception e) {
-      log.error(
-          "Something went wrong while the Token {} for member {} was about to be stored in the database with error {}",
-          token,
-          memberId,
-          e);
-      return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
-    }
-    return ResponseEntity.noContent().build();
-  }
-
-  @GetMapping("/{memberId}/token")
-  public ResponseEntity<?> getFirebaseToken(@PathVariable(name = "memberId") String memberId) {
-    try {
-      Optional<FirebaseToken> firebaseTokenOptional =
-          firebaseNotificationService.getFirebaseToken(memberId);
-      return firebaseTokenOptional
-          .map(firebaseToken -> ResponseEntity.ok(firebaseToken.token))
-          .orElseGet(() -> ResponseEntity.notFound().build());
-    } catch (Exception e) {
-      log.error(
-          "Something went wrong while trying to fetch the firebase token for member {} with error {}",
-          memberId,
-          e);
-      return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
-    }
-  }
-
-  @PostMapping("/{memberId}/push/send")
-  public ResponseEntity<?> sendPushNotification(@PathVariable(name = "memberId") String memberId) {
-    firebaseNotificationService.sendNewMessageNotification(memberId);
     return ResponseEntity.noContent().build();
   }
 }
