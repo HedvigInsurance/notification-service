3d2cbff53ea6418833ba367db7d8ae162faff6a0
meletios <meletios@hedvig.com>
2018-11-02 15:37:59 +0100

    fixes for SendSignedAndActivatedEmail

--- src/main/java/com/hedvig/notificationService/service/NotificationServiceImpl.java
+++ src/main/java/com/hedvig/notificationService/service/NotificationServiceImpl.java
@@ -27,7 +27,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestBody;
 
 @Service
 public class NotificationServiceImpl implements NotificationService {
