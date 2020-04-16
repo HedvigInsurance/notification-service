8daa4af5104bf2838b0178fc230f9d2a83635d6e
Johan Tjelldén <johan@hedvig.com>
2020-04-16 21:32:44 +0200

    Refactored event creation logic

--- src/main/java/com/hedvig/notificationService/customerio/CustomerioEventCreatorImpl.kt
+++ src/main/java/com/hedvig/notificationService/customerio/CustomerioEventCreatorImpl.kt
@@ -9,27 +9,39 @@ class CustomerioEventCreatorImpl(private val productPricingFacade: ProductPricin
 
         val returnMap = mutableMapOf<String, Any?>("name" to "TmpSignedInsuranceEvent")
         contracts.forEach { contract ->
-            if (contract.type == AgreementType.NorwegianHomeContent) {
-                returnMap["is_signed_innbo"] = true
-                if (contract.startDate != null) {
-                    returnMap["activation_date_innbo"] = contract.startDate.format(DateTimeFormatter.ISO_DATE)
-                }
-                if (contract.switcherCompany != null) {
-                    returnMap["is_switcher_innbo"] = true
-                    returnMap["switcher_company_innbo"] = contract.switcherCompany
-                }
-            } else {
-                returnMap["is_signed_reise"] = true
-                if (contract.startDate != null) {
-                    returnMap["activation_date_reise"] = contract.startDate.format(DateTimeFormatter.ISO_DATE)
-                }
-                if (contract.switcherCompany != null) {
-                    returnMap["is_switcher_reise"] = true
-                    returnMap["switcher_company_reise"] = contract.switcherCompany
-                }
+
+            val type = when (contract.type) {
+                AgreementType.NorwegianHomeContent -> "innbo"
+                AgreementType.NorwegianTravel -> "reise"
+                else -> TODO()
             }
+
+            returnMap["is_signed_$type"] = true
+            updateActivationDate(contract, returnMap, type)
+            updateSwitcherInfo(contract, returnMap, type)
         }
 
         return returnMap.toMap()
+    }
+
+    private fun updateSwitcherInfo(
+        contract: ContractInfo,
+        returnMap: MutableMap<String, Any?>,
+        type: String
+    ) {
+        if (contract.switcherCompany != null) {
+            returnMap["is_switcher_$type"] = true
+            returnMap["switcher_company_$type"] = contract.switcherCompany
+        }
+    }
+
+    private fun updateActivationDate(
+        contract: ContractInfo,
+        returnMap: MutableMap<String, Any?>,
+        type: String
+    ) {
+        if (contract.startDate != null) {
+            returnMap["activation_date_$type"] = contract.startDate.format(DateTimeFormatter.ISO_DATE)
+        }
     }
 }
