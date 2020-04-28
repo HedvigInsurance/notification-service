cf0bf8a8c456bb9b3a634f85692945c7b679f4c5
Johan Tjelldén <johan@hedvig.com>
2020-04-28 15:50:41 +0200

    Updated debugcontroller

--- src/main/java/com/hedvig/notificationService/customerio/web/DebugController.kt
+++ src/main/java/com/hedvig/notificationService/customerio/web/DebugController.kt
@@ -1,12 +1,17 @@
 package com.hedvig.notificationService.customerio.web
 
 import com.hedvig.customerio.CustomerioClient
+import com.hedvig.notificationService.customerio.AgreementType
+import com.hedvig.notificationService.customerio.ContractInfo
 import com.hedvig.notificationService.customerio.Workspace
+import com.hedvig.notificationService.customerio.events.CustomerioEventCreatorImpl
+import com.hedvig.notificationService.customerio.state.CustomerioState
 import org.springframework.context.annotation.Profile
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.time.LocalDate
 
 @RestController
 @RequestMapping("/_/customerio/debug")
@@ -18,16 +23,58 @@ class DebugController(val clients: Map<Workspace, CustomerioClient>) {
 
         clients[Workspace.NORWAY]?.sendEvent(
             memberId,
+            CustomerioEventCreatorImpl().contractCreatedEvent(
+                CustomerioState(memberId, null),
+                listOf(
+                    ContractInfo(AgreementType.NorwegianHomeContent, "IF", LocalDate.parse("2020-05-01")),
+                    ContractInfo(AgreementType.NorwegianTravel, "Folksam", null)
+                )
+            )
+        )
+    }
+
+    @PostMapping("{memberId}/activationDateUpdatedEvent")
+    fun activationDateUpdatedEvent(@PathVariable memberId: String) {
+
+        clients[Workspace.NORWAY]?.sendEvent(
+            memberId,
             mapOf<String, Any?>(
-                "name" to "TestSignEvent", "data" to mapOf<String, Any?>(
-                    "contracts" to listOf(
+                "name" to "ActivationDateUpdatedEvent", "data" to mapOf<String, Any?>(
+                    "contractsWithStartDate" to listOf(
+                        mapOf(
+                            "type" to "innbo",
+                            "switcher_company" to "IF",
+                            "activation_date" to "2020-05-01"
+                        )
+                    ),
+                    "contractsWithoutStartDate" to listOf(
+                        mapOf(
+                            "type" to "reise",
+                            "switcher_company" to "IF"
+                        )
+                    )
+                )
+            )
+        )
+    }
+
+    @PostMapping("{memberId}/activationDateTodayEvent")
+    fun activationDateTodayEvent(@PathVariable memberId: String) {
+
+        clients[Workspace.NORWAY]?.sendEvent(
+            memberId,
+            mapOf<String, Any?>(
+                "name" to "ActivationDateTodayEvent", "data" to mapOf<String, Any?>(
+                    "activeToday" to listOf(
                         mapOf(
                             "type" to "innbo",
                             "switcher_company" to "IF"
-                        ),
+                        )
+                    ),
+                    "activeInFuture" to listOf(
                         mapOf(
                             "type" to "reise",
-                            "activation_date" to "2020-05-01"
+                            "switcher_company" to "IF"
                         )
                     )
                 )
