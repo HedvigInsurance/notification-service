ee6871844e92f5ee8fea8bf12b7e15cddde02634
Johan Tjelldén <johan@hedvig.com>
2020-06-01 15:12:17 +0200

    Test simplifications

--- src/test/java/com/hedvig/notificationService/customerio/state/JDBIRepositoryTest.kt
+++ src/test/java/com/hedvig/notificationService/customerio/state/JDBIRepositoryTest.kt
@@ -4,17 +4,18 @@ import assertk.assertThat
 import assertk.assertions.isEqualTo
 import com.hedvig.notificationService.configuration.JDBIConfiguration
 import org.jdbi.v3.core.Jdbi
-import org.junit.Test
-import org.junit.runner.RunWith
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
 import org.springframework.boot.test.context.ConfigFileApplicationContextInitializer
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.ContextConfiguration
-import org.springframework.test.context.junit4.SpringRunner
+import org.springframework.test.context.junit.jupiter.SpringExtension
 
-@RunWith(SpringRunner::class)
+@ExtendWith(SpringExtension::class)
 @DataJdbcTest()
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @ActiveProfiles("test")
@@ -22,16 +23,20 @@ import org.springframework.test.context.junit4.SpringRunner
     classes = [JDBIConfiguration::class],
     initializers = [ConfigFileApplicationContextInitializer::class]
 )
-class JDBIRepositoryTest {
+class JDBIRepositoryTest(@Autowired val jdbi: Jdbi) {
 
-    @Autowired
-    lateinit var jdbi: Jdbi
+    lateinit var repository: JDBIRepository
+
+    @BeforeEach
+    fun setup() {
+        repository = JDBIRepository(jdbi)
+    }
 
     @Test
     fun `simple jdbi query test`() {
-        val result = jdbi.withHandle<Integer, RuntimeException> { it ->
+        val result = jdbi.withHandle<Int, RuntimeException> { it ->
             it.createQuery("select 1 as number")
-                .mapTo(Integer::class.java).findOnly()
+                .mapTo(Int::class.java).first()
         }
 
         assertThat(result).isEqualTo(1)
@@ -39,12 +44,12 @@ class JDBIRepositoryTest {
 
     @Test
     fun `after insert repo row count is 1`() {
-        val repository = JDBIRepository(jdbi)
 
-        repository.save(CustomerioState("aMemberId"))
+        val state = makeCustomerioState()
+        repository.save(state)
 
-        var rows = jdbi.withHandle<Integer, java.lang.RuntimeException> {
-            it.createQuery("select count(*) from customerio_state").mapTo(Integer::class.java).first()
+        val rows = jdbi.withHandle<Int, java.lang.RuntimeException> {
+            it.createQuery("select count(*) from customerio_state").mapTo(Int::class.java).first()
         }
 
         assertThat(rows).isEqualTo(1)
@@ -52,14 +57,13 @@ class JDBIRepositoryTest {
 
     @Test
     fun `after insert two saves repo row count is 1`() {
-        val repository = JDBIRepository(jdbi)
 
-        val state = CustomerioState("aMemberId")
+        val state = makeCustomerioState()
         repository.save(state)
         repository.save(state)
 
-        var rows = jdbi.withHandle<Integer, java.lang.RuntimeException> {
-            it.createQuery("select count(*) from customerio_state").mapTo(Integer::class.java).first()
+        val rows = jdbi.withHandle<Int, java.lang.RuntimeException> {
+            it.createQuery("select count(*) from customerio_state").mapTo(Int::class.java).first()
         }
 
         assertThat(rows).isEqualTo(1)
@@ -67,16 +71,15 @@ class JDBIRepositoryTest {
 
     @Test
     fun `verify memberId is persisted`() {
-        val repository = JDBIRepository(jdbi)
 
-        val state = CustomerioState("1337")
+        val state = makeCustomerioState("1337")
         repository.save(state)
 
-        var memberId = jdbi.withHandle<String, RuntimeException> {
+        val memberId = jdbi.withHandle<String, RuntimeException> {
             it.createQuery("select member_id from customerio_state").mapTo(String::class.java).first()
         }
 
-        assertThat(memberId).isEqualTo(state.memberId)
+        assertThat(memberId).isEqualTo("1337")
     }
     /**
      *
@@ -87,4 +90,8 @@ class JDBIRepositoryTest {
      * implement unsaved type for ID field
      *
      */
+}
+
+fun makeCustomerioState(memberId: String = "1338"): CustomerioState {
+    return CustomerioState(memberId)
 }
