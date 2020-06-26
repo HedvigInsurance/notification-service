dcac9547adbe203aaff678f511a6357ec6aeaefb
Johan Tjelldén <johan@hedvig.com>
2020-06-26 14:27:25 +0200

    Fix broken unit test

--- src/test/java/com/hedvig/notificationService/customerio/web/OnContractRenewalQuuedEventTest.kt
+++ src/test/java/com/hedvig/notificationService/customerio/web/OnContractRenewalQuuedEventTest.kt
@@ -7,17 +7,23 @@ import com.hedvig.notificationService.customerio.EventHandler
 import com.hedvig.notificationService.customerio.dto.ContractRenewalQueuedEvent
 import com.hedvig.notificationService.customerio.state.CustomerioState
 import com.hedvig.notificationService.customerio.state.InMemoryCustomerIOStateRepository
+import io.mockk.mockk
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import java.time.Instant
 
 class OnContractRenewalQuuedEventTest {
 
     val repo = InMemoryCustomerIOStateRepository()
+    lateinit var sut: EventHandler
+
+    @BeforeEach
+    fun setup() {
+        sut = EventHandler(repo, ConfigurationProperties(), mapOf(), mockk(), mockk())
+    }
 
     @Test
     fun `contractRenewalQueuedTriggerAt set`() {
-        val sut = EventHandler(repo, ConfigurationProperties())
-
         val aInstant = Instant.now()
         sut.onContractRenewalQueued(ContractRenewalQueuedEvent("aContract", "1337"), aInstant)
 
@@ -27,8 +33,6 @@ class OnContractRenewalQuuedEventTest {
 
     @Test
     fun `existing contract exists`() {
-        val sut = EventHandler(repo, ConfigurationProperties())
-
         val customerioState = CustomerioState("1337")
         customerioState.createContract("aContract", Instant.now(), null)
         repo.save(customerioState)
@@ -42,7 +46,6 @@ class OnContractRenewalQuuedEventTest {
 
     @Test
     fun `existing contract with other id exists`() {
-        val sut = EventHandler(repo, ConfigurationProperties())
 
         val customerioState = CustomerioState("1337")
         customerioState.createContract("aOtherContract", Instant.now(), null)
