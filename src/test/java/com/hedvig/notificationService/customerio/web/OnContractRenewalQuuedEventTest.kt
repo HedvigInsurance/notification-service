77bbb13402d308259e177c5d4d7cbe3c07646a61
meletios <meletios@hedvig.com>
2020-07-08 15:34:13 +0200

    update tests

--- src/test/java/com/hedvig/notificationService/customerio/web/OnContractRenewalQuuedEventTest.kt
+++ src/test/java/com/hedvig/notificationService/customerio/web/OnContractRenewalQuuedEventTest.kt
@@ -3,64 +3,52 @@ package com.hedvig.notificationService.customerio.web
 import assertk.assertThat
 import assertk.assertions.isEqualTo
 import com.hedvig.notificationService.customerio.ConfigurationProperties
+import com.hedvig.notificationService.customerio.CustomerioService
 import com.hedvig.notificationService.customerio.EventHandler
+import com.hedvig.notificationService.customerio.dto.ChargeFailedEvent
 import com.hedvig.notificationService.customerio.dto.ContractRenewalQueuedEvent
+import com.hedvig.notificationService.customerio.dto.objects.ChargeFailedReason
+import com.hedvig.notificationService.customerio.hedvigfacades.MemberServiceImpl
 import com.hedvig.notificationService.customerio.state.CustomerioState
 import com.hedvig.notificationService.customerio.state.InMemoryCustomerIOStateRepository
+import com.hedvig.notificationService.service.FirebaseNotificationService
 import io.mockk.mockk
+import io.mockk.slot
+import io.mockk.verify
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import java.time.Instant
+import java.time.LocalDate
 
 class OnContractRenewalQuuedEventTest {
 
-    val repo = InMemoryCustomerIOStateRepository()
-    lateinit var sut: EventHandler
+    @Test
+    internal fun `renewal queued test`() {
+        val configurationProperties = ConfigurationProperties()
+        configurationProperties.useNorwayHack = false
+        val customerioService = mockk<CustomerioService>(relaxed = true)
+        val memberService = mockk<MemberServiceImpl>()
+        val firebaseNotificationService = mockk<FirebaseNotificationService>(relaxed = true)
 
-    @BeforeEach
-    fun setup() {
-        sut = EventHandler(
+        val repo = InMemoryCustomerIOStateRepository()
+        val sut = EventHandler(
             repo = repo,
-            configuration = ConfigurationProperties(),
-            firebaseNotificationService = mockk(),
-            customerioService = mockk(),
-            memberService = mockk()
+            configuration = configurationProperties,
+            firebaseNotificationService = firebaseNotificationService,
+            customerioService = customerioService,
+            memberService = memberService
         )
-    }
 
-    @Test
-    fun `contractRenewalQueuedTriggerAt set`() {
-        val aInstant = Instant.now()
-        sut.onContractRenewalQueued(ContractRenewalQueuedEvent("aContract", "1337"), aInstant)
+        sut.onContractRenewalQueued(
+            ContractRenewalQueuedEvent(
+                "contractOne",
+                "member",
+                LocalDate.of(1989,2,17)
+            )
+        )
 
-        val contract = repo.data["1337"]!!.contracts.find { it.contractId == "aContract" }
-        assertThat(contract!!.contractRenewalQueuedTriggerAt).isEqualTo(aInstant)
-    }
-
-    @Test
-    fun `existing contract exists`() {
-        val customerioState = CustomerioState("1337")
-        customerioState.createContract("aContract", Instant.now(), null)
-        repo.save(customerioState)
-
-        val aInstant = Instant.now()
-        sut.onContractRenewalQueued(ContractRenewalQueuedEvent("aContract", "1337"), aInstant)
-
-        val contract = repo.data["1337"]!!.contracts.find { it.contractId == "aContract" }
-        assertThat(contract!!.contractRenewalQueuedTriggerAt).isEqualTo(aInstant)
-    }
-
-    @Test
-    fun `existing contract with other id exists`() {
-
-        val customerioState = CustomerioState("1337")
-        customerioState.createContract("aOtherContract", Instant.now(), null)
-        repo.save(customerioState)
-
-        val aInstant = Instant.now()
-        sut.onContractRenewalQueued(ContractRenewalQueuedEvent("aContract", "1337"), aInstant)
-
-        val contract = repo.data["1337"]!!.contracts.find { it.contractId == "aContract" }
-        assertThat(contract!!.contractRenewalQueuedTriggerAt).isEqualTo(aInstant)
+        val slot = slot<Map<String, Any>>()
+        verify { customerioService.sendEvent("member", capture(slot)) }
+        assertThat(slot.captured["name"]).isEqualTo("ContractRenewalQueuedEvent")
     }
 }
