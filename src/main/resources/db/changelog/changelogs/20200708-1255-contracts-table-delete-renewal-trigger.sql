9194e18cf36ae8bfb01623d9f4cd6a39a6380f13
meletios <meletios@hedvig.com>
2020-07-08 15:31:39 +0200

    remove trigger from the database

---
+++ src/main/resources/db/changelog/changelogs/20200708-1255-contracts-table-delete-renewal-trigger.sql
@@ -0,0 +1,9 @@
+--liquibase formatted.sql
+
+--changeset meletis:20200708-1255-contracts-table-delete-renewal-trigger.sql
+
+
+ALTER TABLE contract_state
+    DROP COLUMN contract_renewal_queued_trigger_at
+
+--rollback DROP TABLE contract_state
