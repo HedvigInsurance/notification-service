c5290e0efcd8cfa45cad91355d901ed9bb847320
meletios <meletios@hedvig.com>
2020-07-08 15:33:56 +0200

    add new fields to the renewal event

--- src/main/java/com/hedvig/notificationService/customerio/dto/ContractRenewalQueuedEvent.kt
+++ src/main/java/com/hedvig/notificationService/customerio/dto/ContractRenewalQueuedEvent.kt
@@ -1,6 +1,18 @@
 package com.hedvig.notificationService.customerio.dto
 
+import java.time.LocalDate
+
 class ContractRenewalQueuedEvent(
     val contractId: String,
-    val memberId: String
-)
+    val memberId: String,
+    val renewalQueuedAt: LocalDate
+) {
+    fun toMap() = mapOf(
+        "name" to "ContractRenewalQueuedEvent",
+        "data" to mapOf(
+            "member_id" to memberId,
+            "contract_id" to contractId,
+            "renewal_queued_at" to renewalQueuedAt
+        )
+    )
+}
