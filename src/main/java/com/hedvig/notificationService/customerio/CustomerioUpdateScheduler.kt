9095d2f3068955b98c2d3321701bbc5e73becbbb
Fredrik Areschoug <fredrik.areschoug@gmail.com>
2020-08-04 10:17:56 +0200

    scheduler quartz job bean

--- src/main/java/com/hedvig/notificationService/customerio/CustomerioUpdateScheduler.kt
+++ src/main/java/com/hedvig/notificationService/customerio/CustomerioUpdateScheduler.kt
@@ -3,42 +3,23 @@ package com.hedvig.notificationService.customerio
 import com.hedvig.notificationService.customerio.customerioEvents.CustomerioEventCreator
 import com.hedvig.notificationService.customerio.hedvigfacades.ContractLoader
 import com.hedvig.notificationService.customerio.state.CustomerIOStateRepository
-import org.quartz.Job
 import org.quartz.JobExecutionContext
 import org.slf4j.LoggerFactory
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.context.annotation.Bean
-import org.springframework.scheduling.annotation.Scheduled
-import org.springframework.stereotype.Component
+import org.springframework.scheduling.quartz.QuartzJobBean
 import java.time.Instant
 import java.time.temporal.ChronoUnit
 
 const val SIGN_EVENT_WINDOWS_SIZE_MINUTES = 10L
 
-open class CustomerioUpdateScheduler: Job {
-
-    //TODO: This are still null :(
-    lateinit var eventCreator: CustomerioEventCreator
-    lateinit var stateRepository: CustomerIOStateRepository
-    lateinit var contractLoader: ContractLoader
-    lateinit var customerioService: CustomerioService
+open class CustomerioUpdateScheduler(
+    private val eventCreator: CustomerioEventCreator,
+    private val stateRepository: CustomerIOStateRepository,
+    private val contractLoader: ContractLoader,
+    private val customerioService: CustomerioService
+): QuartzJobBean() {
 
     private val logger =
         LoggerFactory.getLogger(CustomerioUpdateScheduler::class.java)
-
-    constructor()
-
-    constructor(
-        eventCreator: CustomerioEventCreator,
-        stateRepository: CustomerIOStateRepository,
-        contractLoader: ContractLoader,
-        customerioService: CustomerioService
-    ) {
-        this.eventCreator = eventCreator
-        this.stateRepository = stateRepository
-        this.contractLoader = contractLoader
-        this.customerioService = customerioService
-    }
 
     fun sendUpdates(timeNow: Instant = Instant.now()) {
         val windowEndTime = timeNow.minus(
@@ -58,7 +39,7 @@ open class CustomerioUpdateScheduler: Job {
         }
     }
 
-    override fun execute(context: JobExecutionContext) {
+    override fun executeInternal(context: JobExecutionContext) {
         sendUpdates()
     }
 }
