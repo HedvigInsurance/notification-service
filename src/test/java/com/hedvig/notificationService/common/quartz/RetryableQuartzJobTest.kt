e39f508e569a68e07a3de3b754546a57a60052fb
Johan Tjelldén <johan@hedvig.com>
2020-08-06 11:16:11 +0200

    Call error function with exception happens during rescheduling

--- src/test/java/com/hedvig/notificationService/common/quartz/RetryableQuartzJobTest.kt
+++ src/test/java/com/hedvig/notificationService/common/quartz/RetryableQuartzJobTest.kt
@@ -15,6 +15,7 @@ import org.quartz.JobDataMap
 import org.quartz.JobDetail
 import org.quartz.JobExecutionContext
 import org.quartz.Scheduler
+import org.quartz.SchedulerException
 import org.quartz.Trigger
 import org.quartz.TriggerBuilder
 import org.quartz.impl.JobExecutionContextImpl
@@ -134,6 +135,28 @@ class RetryableQuartzJobTest {
         assertThat(errorLambdaCalled).isTrue()
     }
 
+    @Test
+    fun `call error function if exception happens during scheduling`() {
+        val scheduler = mockk<Scheduler>()
+        val job = TestableJob()
+
+        val jobData = JobDataMap()
+        jobData.putAsString("RETRY_COUNT", 1)
+
+        val jobContext = makeJobExecutionContext(scheduler, job, jobData)
+
+        every { scheduler.scheduleJob(any(), any()) } throws SchedulerException()
+
+        var errorLambdaCalled = false
+        executeWithRetry(jobContext, {
+            errorLambdaCalled = true
+        }) {
+            throw RuntimeException()
+        }
+
+        assertThat(errorLambdaCalled).isTrue()
+    }
+
     val MAX_RETRIES = 5
     private fun executeWithRetry(
         context: JobExecutionContextImpl,
@@ -143,21 +166,28 @@ class RetryableQuartzJobTest {
         try {
             function()
         } catch (e: RuntimeException) {
-
-            val originalStartTime = context.trigger.startTime.toInstant()
-            val newStartTime = Date.from(originalStartTime.plus(1, ChronoUnit.MINUTES))
-
             val retryCount = context.jobDetail.jobDataMap.getIntOrNull("RETRY_COUNT") ?: 0
             context.jobDetail.jobDataMap.putAsString("RETRY_COUNT", retryCount + 1)
 
             if (retryCount < MAX_RETRIES) {
-                context.scheduler.scheduleJob(
-                    context.jobDetail,
-                    TriggerBuilder.newTrigger().startAt(newStartTime).build()
-                )
+                rescheduleJob(context, errorFunction)
             } else {
                 errorFunction()
             }
+        }
+    }
+
+    private fun rescheduleJob(context: JobExecutionContextImpl, errorFunction: () -> Unit) {
+        try {
+            val originalStartTime = context.trigger.startTime.toInstant()
+            val newStartTime = Date.from(originalStartTime.plus(1, ChronoUnit.MINUTES))
+
+            context.scheduler.scheduleJob(
+                context.jobDetail,
+                TriggerBuilder.newTrigger().startAt(newStartTime).build()
+            )
+        } catch (ex: SchedulerException) {
+            errorFunction()
         }
     }
 }
