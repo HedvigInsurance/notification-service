c78a7d652cf931d2612bab71810c959064bb74a1
Johan Tjelldén <johan@hedvig.com>
2020-08-06 16:14:44 +0200

    Test ContractCreatedJobLogic with old tests

--- src/test/java/com/hedvig/notificationService/customerio/web/ContractCreatedEventCreatesContractTest.kt
+++ src/test/java/com/hedvig/notificationService/customerio/web/ContractCreatedEventCreatesContractTest.kt
@@ -29,7 +29,7 @@ class ContractCreatedEventCreatesContractTest {
     val firebaseNotificationService = mockk<FirebaseNotificationService>()
     val customerioService = mockk<CustomerioService>()
     val memberService = mockk<MemberServiceImpl>()
-    val scheduler: Scheduler = mockk()
+    val scheduler: Scheduler = mockk(relaxed = true)
     val sut = EventHandler(
         repo = repo,
         firebaseNotificationService = firebaseNotificationService,
