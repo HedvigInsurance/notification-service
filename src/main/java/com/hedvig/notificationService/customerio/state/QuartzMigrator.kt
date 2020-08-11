a654ebf1a97f86c739b2d13e07e3ee2ef2086f45
Johan Tjelldén <johan@hedvig.com>
2020-08-11 09:48:51 +0200

    Add logging and tracing to QuartzMigrator

--- src/main/java/com/hedvig/notificationService/customerio/state/QuartzMigrator.kt
+++ src/main/java/com/hedvig/notificationService/customerio/state/QuartzMigrator.kt
@@ -2,6 +2,8 @@ package com.hedvig.notificationService.customerio.state
 
 import com.hedvig.notificationService.customerio.customerioEvents.jobs.JobScheduler
 import com.hedvig.notificationService.customerio.hedvigfacades.ContractLoader
+import datadog.trace.api.Trace
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 import java.time.Instant
 import java.time.ZoneId
@@ -14,45 +16,58 @@ class QuartzMigrator(
     private val jobScheduler: JobScheduler,
     private val contractLoader: ContractLoader
 ) {
+    val log = LoggerFactory.getLogger(this::class.java)
+
     @Transactional
     fun migrate(now: Instant) {
         val statesToMigrate = repo.statesWithTriggers()
 
         for (state in statesToMigrate) {
-            val contracts = contractLoader.getContractInfoForMember(state.memberId)
-
-            if (state.startDateUpdatedTriggerAt != null) {
-
-                jobScheduler.rescheduleOrTriggerStartDateUpdated(
-                    now, state.memberId
-                )
-                state.sentStartDateUpdatedEvent()
-            }
-
-            if (state.contractCreatedTriggerAt != null) {
-                jobScheduler.rescheduleOrTriggerContractCreated(
-                    now, state.memberId
-                )
-                state.sentContractCreatedEvent()
-            }
-
-            if (state.activationDateTriggerAt != null) {
-                for (contract in contracts) {
-                    if (contract.startDate != null && contract.startDate.isAfter(
-                            now.truncatedTo(ChronoUnit.DAYS).atZone(ZoneId.of("Europe/Stockholm")).toLocalDate()
-                        )
-                    ) {
-                        jobScheduler.rescheduleOrTriggerContractActivatedToday(
-                            activationDate = contract.startDate,
-                            memberId = state.memberId,
-                            contractId = contract.contractId.toString()
-                        )
-                    }
-                }
-                state.sentActivatesTodayEvent(null)
-            }
+            migrateMember(state, now)
 
             repo.save(state)
+        }
+    }
+
+    @Trace
+    private fun migrateMember(
+        state: CustomerioState,
+        now: Instant
+    ) {
+        log.info("Migrating ${state.memberId}")
+        val contracts = contractLoader.getContractInfoForMember(state.memberId)
+
+        if (state.startDateUpdatedTriggerAt != null) {
+            log.info("Migrating startDateUpdatedTriggerAt for member ${state.memberId}")
+            jobScheduler.rescheduleOrTriggerStartDateUpdated(
+                now, state.memberId
+            )
+            state.sentStartDateUpdatedEvent()
+        }
+
+        if (state.contractCreatedTriggerAt != null) {
+            log.info("Migrating rescheduleOrTriggerContractCreated for member ${state.memberId}")
+            jobScheduler.rescheduleOrTriggerContractCreated(
+                now, state.memberId
+            )
+            state.sentContractCreatedEvent()
+        }
+
+        if (state.activationDateTriggerAt != null) {
+            for (contract in contracts) {
+                if (contract.startDate != null && contract.startDate.isAfter(
+                        now.truncatedTo(ChronoUnit.DAYS).atZone(ZoneId.of("Europe/Stockholm")).toLocalDate()
+                    )
+                ) {
+                    log.info("Migrating startDate for member ${state.memberId} and contract ${contract.contractId}")
+                    jobScheduler.rescheduleOrTriggerContractActivatedToday(
+                        activationDate = contract.startDate,
+                        memberId = state.memberId,
+                        contractId = contract.contractId.toString()
+                    )
+                }
+            }
+            state.sentActivatesTodayEvent(null)
         }
     }
 }
